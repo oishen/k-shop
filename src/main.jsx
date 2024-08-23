cafeb646faa652aad7593f2ddaf5883f3581cc43
Soy Zen <noobiezz2020@gmail.com>
2024-08-23 16:35:13 +0700

    add

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import ShopContextProvider from './Component/ShopContext/ShopContext.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <ShopContextProvider>
