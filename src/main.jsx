637e9e64c3c1ebf7ca4675860534d8cc0521c515
Soy Zen <noobiezz2020@gmail.com>
2024-08-30 09:29:02 +0700

    change bootstrap file

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import ShopContextProvider from './Component/ShopContext/ShopContext.jsx'
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <ShopContextProvider>
