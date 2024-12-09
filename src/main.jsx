94a913b6514ef497ab4dbe8101c760ce897b2a1d
Soy Zen <125181005+oishen@users.noreply.github.com>
2024-12-09 20:07:02 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import ShopContextProvider from './Component/ShopContext/ShopContext.jsx'
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js"
+import "./bootstrap/dist/js/bootstrap.bundle.js"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <ShopContextProvider>
